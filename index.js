2f88d5337b7c7724df7b65ae4e21493a9760e5ec
Cássia ☆ <102737875+cassiaplima@users.noreply.github.com>
2022-05-05 08:59:34 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+//Exercicios- Cassia Pereira Lima
+//1-a
+console.log(10+20*30);
+
+//1-b
+console.log(4**2/30);
+
+//1-c
+console.log((9**4+2)*6-1);
+
+//2-a
+console.log(10%3*10**1+1-10*4/2);
+
+//3-a 
+console.log("Cassia");
+
+//4-a
+console.log(2*3*3*5);
+
+//5-a
+const a = 2;
+const b = 4;
+const c = 6;
+console.log(a+b+c);
+
+//6-a
+console.log(1800*2);
+
+//Exemplos
+const nome = "cassia";
+console.log(nome);
+
+const d = 3;
+const e = 2;
+
+console.log(d+e)
